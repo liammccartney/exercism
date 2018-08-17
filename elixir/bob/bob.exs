9d1cfd124cfbb5ac85419295e1c4b4b136948661
Liam McCartney <liam.mccartney@gmail.com>
2018-08-17 07:45:57 -0400

    wip

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,18 @@
 defmodule Bob do
   def hey(input) do
+    letters = String.replace(input, ~r/[^a-zA-Z]/, "")
+    IO.puts(letters)
     cond do
-      true -> raise "Your implementation goes here"
+      String.trim(input) === "" ->
+        "Fine. Be that way!"
+      String.upcase(letters) === letters and String.ends_with?(input, "?") ->
+        "Calm down, I know what I'm doing!"
+      String.upcase(letters) === letters ->
+        "Whoa, chill out!"
+      String.ends_with?(input, "?") ->
+        "Sure."
+      true -> 
+        "Whatever."
     end
   end
 end
