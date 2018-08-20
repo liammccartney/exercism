eccd059353578955c508eb70fdfab081455900aa
Liam McCartney <liam.mccartney@gmail.com>
2018-08-19 21:36:10 -0400

    Complete (?) nucleotides exercise

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -14,6 +14,13 @@ defmodule NucleotideCount do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
+    Enum.reduce(strand, 0, fn (x, y) ->
+      if x == nucleotide do
+        y + 1
+      else
+        y
+      end
+    end)
   end
 
   @doc """
@@ -26,5 +33,9 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
+    %{?A => count(strand, ?A),
+      ?T => count(strand, ?T),
+      ?C => count(strand, ?C),
+      ?G => count(strand, ?G)}
   end
 end
