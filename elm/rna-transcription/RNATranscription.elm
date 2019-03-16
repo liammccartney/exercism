8b6c93e5be638a83eaf3fa20be527c8855e2b45e
Liam McCartney <liam.mccartney@gmail.com>
2019-03-16 14:16:29 -0400

    [Elm] -- RNA Challenge WIP

--- elm/rna-transcription/RNATranscription.elm
+++ elm/rna-transcription/RNATranscription.elm
@@ -1,6 +1,30 @@
 module RNATranscription exposing (toRNA)
 
+import Dict
+import Result exposing (..)
+import String
+
+
+transform : Char -> Result Char Char
+transform x =
+    case x of
+        'G' ->
+            Ok 'C'
+
+        'C' ->
+            Ok 'G'
+
+        'T' ->
+            Ok 'A'
+
+        'A' ->
+            Ok 'U'
+
+        _ ->
+            Err x
+
 
 toRNA : String -> Result Char String
 toRNA dna =
-    Debug.todo "Please implement this function"
+    dna
+        |> String.map (\n -> Result.map transform n)
