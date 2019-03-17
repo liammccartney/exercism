08c371c4c84ea35e2d587a23aa89d1f42c216b70
Liam McCartney <liam.mccartney@gmail.com>
2019-03-17 17:25:48 -0400

    [Elm] -- Complete WordCount challenge

--- elm/word-count/WordCount.elm
+++ elm/word-count/WordCount.elm
@@ -1,8 +1,36 @@
 module WordCount exposing (wordCount)
 
 import Dict exposing (Dict)
+import List
+import Regex
+
+
+punctuation : Regex.Regex
+punctuation =
+    Maybe.withDefault Regex.never <|
+        Regex.fromString "[^a-zA-Z0-9\\s]+"
+
+
+reduct : String -> Dict String Int -> Dict String Int
+reduct word acc =
+    Dict.update word update acc
+
+
+update : Maybe Int -> Maybe Int
+update count =
+    case count of
+        Nothing ->
+            Just 1
+
+        Just x ->
+            Just (x + 1)
 
 
 wordCount : String -> Dict String Int
 wordCount sentence =
-    Debug.todo "Please implement this function"
+    sentence
+        |> String.toLower
+        |> Regex.replace punctuation (\_ -> "")
+        |> String.split " "
+        |> List.filter (\word -> String.length word > 0)
+        |> List.foldl reduct Dict.empty
