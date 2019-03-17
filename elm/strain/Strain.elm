ca9de0b96720af8d852f991d4446f33cb249d1af
Liam McCartney <liam.mccartney@gmail.com>
2019-03-17 16:38:46 -0400

    [Elm] -- Complete Strain challenge

--- elm/strain/Strain.elm
+++ elm/strain/Strain.elm
@@ -1,11 +1,31 @@
 module Strain exposing (discard, keep)
 
+import List
+
 
 keep : (a -> Bool) -> List a -> List a
 keep predicate list =
-    Debug.todo "Please implement this function"
+    case list of
+        [] ->
+            []
+
+        hd :: tl ->
+            if predicate hd then
+                hd :: keep predicate tl
+
+            else
+                keep predicate tl
 
 
 discard : (a -> Bool) -> List a -> List a
 discard predicate list =
-    Debug.todo "Please implement this function"
+    case list of
+        [] ->
+            []
+
+        hd :: tl ->
+            if predicate hd then
+                discard predicate tl
+
+            else
+                hd :: discard predicate tl
