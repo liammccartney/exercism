9e7790ba2e8690ffc67d411924df36e4311e9e1c
Liam McCartney <liam.mccartney@gmail.com>
2019-03-25 16:42:28 -0400

    [Elm] -- Complete Triangle
    Honestly this feels like a joke, is there a simpler less exhaustive
    solution I'm not seeing?

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -9,4 +9,17 @@ type Triangle
 
 triangleKind : number -> number -> number -> Result String Triangle
 triangleKind x y z =
-    Debug.todo "Please implement this function"
+    if List.any (\n -> n <= 0) [ x, y, z ] then
+        Err "Invalid lengths"
+
+    else if (x + y) < z || (x + z) < y || (y + z) < x then
+        Err "Violates inequality"
+
+    else if (x == y) && (y == z) then
+        Ok Equilateral
+
+    else if (x == y) || (x == z) || (y == z) then
+        Ok Isosceles
+
+    else
+        Ok Scalene
