e2d58168920efa7b411514c983fb26a013152cd9
Liam McCartney <liam.mccartney@gmail.com>
2019-03-28 14:21:41 -0400

    [Elm] -- Complete Sublist

--- elm/sublist/Sublist.elm
+++ elm/sublist/Sublist.elm
@@ -1,5 +1,7 @@
 module Sublist exposing (ListComparison(..), sublist)
 
+import Array exposing (Array)
+
 
 type ListComparison
     = Equal
@@ -10,4 +12,28 @@ type ListComparison
 
 sublist : List a -> List a -> ListComparison
 sublist alist blist =
-    Debug.todo "Please implement this function"
+    let
+        helper a b returnType =
+            if Array.isEmpty b then
+                Unequal
+
+            else if a == Array.slice 0 (Array.length a) b then
+                returnType
+
+            else
+                helper a (Array.slice 1 (Array.length b) b) returnType
+    in
+    if alist == blist then
+        Equal
+
+    else if List.isEmpty alist then
+        Sublist
+
+    else if List.isEmpty blist then
+        Superlist
+
+    else if List.length alist < List.length blist then
+        helper (Array.fromList alist) (Array.fromList blist) Sublist
+
+    else
+        helper (Array.fromList blist) (Array.fromList alist) Superlist
