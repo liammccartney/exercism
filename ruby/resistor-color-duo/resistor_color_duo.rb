de2939cd7588f4992393368f1a658c5666dd2f2c
Liam McCartney <liam.mccartney@gmail.com>
2020-02-05 17:45:54 -0500

    Complete ResistorColorDuo

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,31 @@
+=begin
+Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+`resistor_color_duo_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-duo` directory.
+
+Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White
+=end
+
+class ResistorColorDuo
+  @@colors = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9,
+  }
+
+  def self.value colors
+    colors.slice(0,2).reduce("") do |val, color|
+      "#{val}#{@@colors[color]}"
+    end.to_i
+  end
+end
+
