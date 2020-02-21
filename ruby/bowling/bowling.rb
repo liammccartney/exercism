9579b640094e4f2c88aa4ccb7e50e6d3041e923a
Liam McCartney <liam.mccartney@gmail.com>
2020-02-21 08:21:59 -0500

    [Ruby] Bowling - WIP

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,8 +1,46 @@
-=begin
-Write your code for the 'Bowling' exercise in this file. Make the tests in
-`bowling_test.rb` pass.
+class Game
+  def initialize
+    @rolls = []
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/bowling` directory.
-=end
+  def roll pins
+    if @rolls.length.odd?
+      last_roll = @rolls.last || 0
+        raise BowlingError if last_roll != 10 && (pins + last_roll) > 10
+    end
 
+    if pins < 0 ||
+        pins > 10
+      raise BowlingError
+    end
+    @rolls << pins
+  end
+
+  def score
+    calculate_score @rolls
+  end
+
+  private
+
+  def calculate_score pins
+    return 0 if pins.empty?
+
+    return pins[0] if pins.length == 1
+
+    if pins.length == 3
+      roll1, roll2, roll3 = pins.slice(0,3)
+      return roll1 + roll2 + roll3 + (calculate_score pins.slice(3..))
+    end
+
+    return pins.first + pins[1] + pins[2] + (calculate_score pins.slice(1..)) if (pins.first == 10)
+
+    roll1, roll2 = pins.slice(0,2)
+
+    return roll1 + roll2 + pins[2] + (calculate_score pins.slice(2..)) if roll1 + roll2 == 10
+
+    return roll1 + roll2 + (calculate_score pins.slice(2..))
+  end
+
+  class BowlingError < StandardError
+  end
+end
