00f34f9a3015494536b4b640dcfa0c633ce529c2
Liam McCartney <liam.mccartney@gmail.com>
2020-02-27 23:46:54 -0500

    [Ruby] Bowling - obey Rubocop (kinda)

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -8,7 +8,7 @@ class Game
   end
 
   def roll pins
-    raise BowlingError, 'Cannot roll less than 0 pins' if pins < 0
+    raise BowlingError, 'Cannot roll less than 0 pins' if pins.negative?
     raise BowlingError, 'Cannot roll more than 10 pins at a time' if pins > 10
     raise BowlingError, 'Cannot roll more than 10 frames' if game_complete?
 
@@ -29,6 +29,7 @@ class Game
   def score
     raise BowlingError, 'Cannot score an empty game' if @rolls.empty?
     raise BowlingError, 'Cannot score an incomplete game' unless game_complete?
+
     calculate_score @rolls
   end
 
@@ -43,24 +44,23 @@ class Game
 
     return pins.first if pins.length == 1
 
-    roll1, roll2, roll3 = pins[0,3]
+    roll1, roll2, roll3 = pins[0, 3]
 
-    if pins.length == 3
-      return roll1 + roll2 + roll3 + (calculate_score pins[3..])
-    end
+    # Last Frame
+    return roll1 + roll2 + roll3 if pins.length == 3
 
-    return roll1 + roll2 + roll3 + (calculate_score pins[1..]) if (pins.first == 10)
+    return roll1 + roll2 + roll3 + (calculate_score pins[1..-1]) if strike?(roll1)
 
-    return roll1 + roll2 + roll3 + (calculate_score pins[2..]) if is_spare?(roll1, roll2)
+    return roll1 + roll2 + roll3 + (calculate_score pins[2..-1]) if spare?(roll1, roll2)
 
-    return roll1 + roll2 + (calculate_score pins[2..])
+    roll1 + roll2 + (calculate_score pins[2..-1])
   end
 
-  def is_strike?(roll)
+  def strike?(roll)
     roll == 10
   end
 
-  def is_spare?(roll1, roll2)
+  def spare?(roll1, roll2)
     roll1 + roll2 == 10
   end
 
@@ -74,8 +74,8 @@ class Game
     end
 
     def add_roll(pins)
-      if !@viable_pins_for_next_roll.include?(pins)
-        raise BowlingError, "Not viable, #{pins} not in #{@viable_pins_for_next_roll}"
+      unless @viable_pins_for_next_roll.include?(pins)
+        raise BowlingError, 'Roll not possible'
       end
 
       @rolls << pins
@@ -98,10 +98,8 @@ class Game
     end
 
     def add_roll(pins)
-      raise BowlingError, 'Frame cannot exceed 30 pins' if (@rolls + [pins]).sum > 30
-
-      if !@viable_pins_for_next_roll.include?(pins)
-        raise BowlingError, "Not viable, #{pins} not in #{@viable_pins_for_next_roll}"
+      unless @viable_pins_for_next_roll.include?(pins)
+        raise BowlingError, 'Roll not possible'
       end
 
       @rolls << pins
