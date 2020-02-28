0b401787165f813d0921d7e94b5121a9b83fd639
Liam McCartney <liam.mccartney@gmail.com>
2020-02-28 10:45:32 -0500

    [Ruby] Raindrops - Complete

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,8 +1,18 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
+module Raindrops
+  extend self
 
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
-
+  def convert(number)
+    result = {
+      3 => "Pling",
+      5 => "Plang",
+      7 => "Plong"
+    }.reduce("") do |str, (factor, raindrop)|
+      if number % factor == 0
+        str += raindrop
+      else
+        str
+      end
+    end
+    result.empty? ? number.to_s : result
+  end
+end
