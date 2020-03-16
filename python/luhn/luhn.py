96b724fcb26065d5b62fe0ba3613c1cba41d6b58
Liam McCartney <liam.mccartney@gmail.com>
2020-03-16 13:07:07 -0400

    [Python] Complete Luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,32 @@
+import re
+
+
 class Luhn:
     def __init__(self, card_num):
-        pass
+        self.card_num = card_num
 
     def valid(self):
-        pass
+        try:
+            card_num = [int(x) for x in re.sub(r"\s+", "", self.card_num)[::-1]]
+
+        except ValueError:
+            return False
+
+        if len(card_num) <= 1:
+            return False
+
+        checksum = card_num[0]
+
+        for idx, num in enumerate(card_num[1:]):
+            if idx % 2 == 0:
+                double = num * 2
+
+                if double > 9:
+                    double -= 9
+
+                checksum += double
+                continue
+
+            checksum += num
+
+        return checksum % 10 == 0
--- python/luhn/luhn_test.py
+++ python/luhn/luhn_test.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 
 from luhn import Luhn
 
