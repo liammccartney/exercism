338e49c1ba66fa1ed2e8125f26dcee90b4e32b8f
Liam McCartney <liam.mccartney@gmail.com>
2020-10-08 10:36:48 -0400

    [C#] Complete Pangram

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-    {
-        throw new NotImplementedException();
+  public static bool IsPangram(string input)
+  {
+    var pattern = new Regex(@"[a-zA-Z]+");
+    MatchCollection matches = pattern.Matches(input.ToLower());
+
+    var letters = "";
+
+    foreach (Match match in matches) {
+      letters = String.Concat(letters, match);
     }
+
+    var uniqueLetters = new HashSet<char>(letters);
+
+    return uniqueLetters.Count == 26;
+  }
 }
