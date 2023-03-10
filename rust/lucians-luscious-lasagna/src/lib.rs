d2191d9c4ed01e1c4d425471ab0c127ad6a98337
Liam McCartney <liam.mccartney@gmail.com>
2023-03-10 10:49:14 -0500

    Rust: Complete Lasagna

--- rust/lucians-luscious-lasagna/src/lib.rs
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -11,5 +11,5 @@ pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    return preparation_time_in_minutes(number_of_layers) + remaining_minutes_in_oven(actual_minutes_in_oven);
+    return preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven;
 }
