6860c248b348849ad1d96df67e3d763deed00b2b
Liam McCartney <liam.mccartney@gmail.com>
2023-03-31 00:08:27 -0400

    Rust: complete matching brackets (yuck)

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,3 +1,37 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!("Check if the string \"{string}\" contains balanced brackets");
+    let mut stack: Vec<char> = vec![];
+
+    for x in string.chars() {
+        match x {
+            '(' => stack.push(x),
+            '[' => stack.push(x),
+            '{' => stack.push(x),
+            ')' => match stack.pop() {
+                Some(y) => {
+                    if y != '(' {
+                        stack.push(x)
+                    }
+                }
+                None => stack.push(x),
+            },
+            ']' => match stack.pop() {
+                Some(y) => {
+                    if y != '[' {
+                        stack.push(x)
+                    }
+                }
+                None => stack.push(x)
+            },
+            '}' => match stack.pop() {
+                Some(y) => {
+                    if y != '{' {
+                        stack.push(x)
+                    }
+                }
+                None => stack.push(x),
+            },
+            _ => (),
+        }
+    }
+    return stack.len() == 0;
 }
