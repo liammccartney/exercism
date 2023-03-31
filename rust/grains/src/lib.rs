96fc7d7ea5cf2c20c7283fcbc5aaf1d28b7eab72
Liam McCartney <liam.mccartney@gmail.com>
2023-03-31 00:54:14 -0400

    Rust: Complete grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {s}");
+    match s {
+        1..=64 => 2u64.pow(s - 1).into(),
+        _ => panic!("Square must be between 1 and 64")
+    }
+    
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).map(square).sum()
 }
