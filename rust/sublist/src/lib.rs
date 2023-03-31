0da05d41923b31fe145252a45fd6f2e916ca8916
Liam McCartney <liam.mccartney@gmail.com>
2023-03-31 00:45:35 -0400

    Rust: copy solution from community

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,4 @@
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -7,5 +8,30 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Comparison::Equal,
+        (0, _) => Comparison::Sublist,
+        (_, 0) => Comparison::Superlist,
+        (m, n) if m > n => {
+            if _first_list.windows(n).any(|win| win == _second_list) {
+                Comparison::Superlist
+            } else {
+                Comparison::Unequal
+            }
+        }
+        (m, n) if m < n => {
+            if _second_list.windows(m).any(|win| win == _first_list) {
+                Comparison::Sublist
+            } else {
+                Comparison::Unequal
+            }
+        }
+        (_, _) => {
+            if _first_list == _second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
+    }
 }
